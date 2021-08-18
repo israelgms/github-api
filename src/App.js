2a316c15510a64c8469a95ee42018f7aad04d017
israelgms <israelgomes13@hotmail.com>
2021-08-18 16:55:20 -0300

    Versão final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-function App() {
+import React from "react";
+import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
+
+const App = () => {
+  const { githubState } = useGithub();
   return (
-  <div className="App"><h1>GitHub API</h1></div>
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Carregando... Por favor aguarde!</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/components/no-search/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const NoSearch = () => {
+  return (
+    <S.Wrapper>
+      <h1>Para continuar pesquise um usuário</h1>
+    </S.Wrapper>
+  );
+};
+
+export default NoSearch;
